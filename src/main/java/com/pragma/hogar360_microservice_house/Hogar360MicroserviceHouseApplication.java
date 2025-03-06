f6fb959493851f5ccea022111564ffba6b16c068
JohanJurado <johanjurado0103@gmail.com>
2025-03-06 18:16:30 -0500

    Create HU1 - Houses Microservice

---
+++ src/main/java/com/pragma/hogar360_microservice_house/Hogar360MicroserviceHouseApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.hogar360_microservice_house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hogar360MicroserviceHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hogar360MicroserviceHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/hogar360_microservice_house/Hogar360MicroserviceHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.hogar360_microservice_house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hogar360MicroserviceHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
