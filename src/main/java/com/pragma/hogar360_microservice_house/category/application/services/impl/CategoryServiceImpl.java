93b4cc4f45cc6491b6b9e33d8b777be6397bdd33
JohanJurado <johanjurado0103@gmail.com>
2025-03-07 16:56:37 -0500

    Application code completed

---
+++ src/main/java/com/pragma/hogar360_microservice_house/category/application/services/impl/CategoryServiceImpl.java
@@ -0,0 +1,25 @@
+package com.pragma.hogar360_microservice_house.category.application.services.impl;
+
+import com.pragma.hogar360_microservice_house.category.application.dtos.request.SaveCategoryRequest;
+import com.pragma.hogar360_microservice_house.category.application.dtos.response.SaveCategoryResponse;
+import com.pragma.hogar360_microservice_house.category.application.mappers.ICategoryDtoMapper;
+import com.pragma.hogar360_microservice_house.category.application.services.ICategoryService;
+import com.pragma.hogar360_microservice_house.category.domain.ports.in.ICategoryServicePort;
+import com.pragma.hogar360_microservice_house.commons.configurations.utils.Constants;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements ICategoryService {
+    private final ICategoryServicePort categoryServicePort;
+    private final ICategoryDtoMapper categoryDtoMapper;
+
+    @Override
+    public SaveCategoryResponse save(SaveCategoryRequest saveCategoryRequest) {
+        categoryServicePort.save(categoryDtoMapper.requestToModel(saveCategoryRequest));
+        return new SaveCategoryResponse(Constants.SAVE_CATEGORY_RESPONSE_MESSAGE, LocalDateTime.now());
+    }
+}
