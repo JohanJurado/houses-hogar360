8b0b066d4bc1ca38466ac57f0b255626580bedaf
JohanJurado <johanjurado0103@gmail.com>
2025-03-15 06:44:49 -0500

    SonarLint Completed

--- src/main/java/com/pragma/hogar360_microservice_house/infraestructure/endpoints/rest/LocationController.java
+++ src/main/java/com/pragma/hogar360_microservice_house/infraestructure/endpoints/rest/LocationController.java
@@ -1,6 +1,5 @@
 package com.pragma.hogar360_microservice_house.infraestructure.endpoints.rest;
 
-import com.pragma.hogar360_microservice_house.application.dtos.request.SaveCategoryRequest;
 import com.pragma.hogar360_microservice_house.application.dtos.request.SaveLocationRequest;
 import com.pragma.hogar360_microservice_house.application.dtos.response.SaveDtoResponses;
 import com.pragma.hogar360_microservice_house.application.services.ILocationService;
