12aac44171e34ae786b023fc62a3cfdf58d89123
JohanJurado <johanjurado0103@gmail.com>
2025-04-22 07:27:04 -0500

    Add id in Response Objects and fix code with sonarlint

--- src/main/java/com/pragma/hogar360_microservice_house/application/dtos/response/HouseResponse.java
+++ src/main/java/com/pragma/hogar360_microservice_house/application/dtos/response/HouseResponse.java
@@ -1,6 +1,7 @@
 package com.pragma.hogar360_microservice_house.application.dtos.response;
 
 public record HouseResponse(
+        Long id,
         String name,
         String description,
         Long bedroomCount,
--- src/main/java/com/pragma/hogar360_microservice_house/application/dtos/response/LocationResponse.java
+++ src/main/java/com/pragma/hogar360_microservice_house/application/dtos/response/LocationResponse.java
@@ -1,6 +1,7 @@
 package com.pragma.hogar360_microservice_house.application.dtos.response;
 
 public record LocationResponse(
+        Long id,
         String neighborhood,
         String nameCity,
         String descriptionCity,
